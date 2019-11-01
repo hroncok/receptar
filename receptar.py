dbbafce8be0ed65dcee3c8b94194e7ee5eb5e84f
Miro Hrončok <miro@hroncok.cz>
2019-11-01 18:56:04 +0100

    Zatím poměrně dost hloupý receptář

---
+++ receptar.py
@@ -0,0 +1,23 @@
+import os.path
+
+
+def nacti_recept(jmeno):
+    """
+    Když této funkci dáš jméno textového souboru ze složky recepty,
+    tak vrátí text receptu jako řetězec.
+    """
+    tenhle_script = __file__
+    adresar_projektu = os.path.dirname(tenhle_script)
+    adresar_receptu = os.path.join(adresar_projektu, 'recepty')
+    cesta_k_receptu = os.path.join(adresar_receptu, jmeno)
+    with open(cesta_k_receptu, 'r') as f:
+        return f.read()
+
+
+NAZVY_RECEPTU = [
+    'chlebova_pochoutka.txt',
+]
+
+
+# Máme zatím jen jeden recept, tak ho prostě ukážeme
+print(nacti_recept(NAZVY_RECEPTU[0]))
